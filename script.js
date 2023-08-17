bef504fdd222c3ef9c9483a6c1a9c7e0fefe1d8d
adwxithc <141938926+adwxithc@users.noreply.github.com>
2023-08-17 19:00:42 +0530

    Add files via upload
    responsive web design

---
+++ script.js
@@ -0,0 +1,15 @@
+let sign=document.getElementById("sign");
+
+sign.addEventListener("click",function(){window.location.href="https://www.starbucks.com/account/signin?ReturnUrl=%2F"});
+
+document.getElementById("join").addEventListener("click",function(){window.location.href="https://www.starbucks.com/account/create"});
+document.getElementById("loaca").addEventListener("click",function(){window.location.href="https://www.starbucks.com/store-locator?map=39.635307,-101.337891,5z"});
+
+document.getElementById("menu").addEventListener("click",function(){window.location.href="https://www.starbucks.com/menu"});
+document.getElementById("rewards").addEventListener("click",function(){window.location.href="https://www.starbucks.com/rewards"});
+document.getElementById("gift").addEventListener("click",function(){window.location.href="https://www.starbucks.com/gift"});
+
+document.getElementById("explor").addEventListener("click",function(){window.location.href="https://www.starbucks.com/rewards/mobile-apps/?_branch_match_id=1216026941383020159&utm_source=marketing&utm_campaign=summer_remix&utm_medium=Marketing&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXLy5JLEoqTc4u1kssKNDLyczL1g%2FOTjYKsXCpSndOAgBEcj0YJgAAAA%3D%3D"});
+document.getElementById("order1").addEventListener("click",function(){window.location.href="https://www.starbucks.com/menu/drinks/cold-drinks"});
+document.getElementById("order2").addEventListener("click",function(){window.location.href="https://www.starbucks.com/menu/product/2123644/iced"});
+document.getElementById("order3").addEventListener("click",function(){window.location.href="https://www.starbucks.com/menu/product/2123236/single"});
